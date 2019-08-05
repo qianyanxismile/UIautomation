a5d5b17a315c4181f0a9d3d1b75f0bfbe0a0dc12
qianyanxi <1363761658@qq.com>
2019-08-05 23:51:07 +0800

    配置文件路径调整，初始化代码放在setClass中执行

--- uiproject/tests/test.py
+++ uiproject/tests/test.py
@@ -1,9 +1,16 @@
+# coding: utf-8
+import os
+from pathlib import Path
+import sys
 import unittest
 import time
 from configparser import ConfigParser
 from appium import webdriver
-from ..conf.appium_config import appium_start
+sys.path.extend([str(Path(__file__).resolve().parents[0]), str(Path(__file__).resolve().parents[1])])
+from conf.appium_config import appium_start
 
+current_path = os.path.dirname(os.path.abspath(__file__))
+config_path = os.path.join(os.path.join(os.path.dirname(current_path), "conf"), "config.ini")
 
 class ProductInformation(unittest.TestCase):
     """
@@ -12,38 +19,34 @@ class ProductInformation(unittest.TestCase):
     """
     # @classmethod,在此类中只进行一次初始化和清理工作
     @classmethod
-    def setUpClass(self):
-        self.driver = appium_start()
+    def setUpClass(cls):
+        cls.driver = appium_start()
         time.sleep(3)
-
-    def test_initial(self):
         cfg = ConfigParser()
-        cfg.read('..conf.config.ini')
-        tel_authorize = cfg.get('tel_authorize')
-        close_reward = cfg.get('close_reward')
-        return tel_authorize, close_reward
+        cfg.read(config_path, encoding="u8")
+        cls.tel_authorize = cfg.get("authorize", 'tel_authorize')
+        cls.close_reward = cfg.get("reward", 'close_reward')
 
     def test_tel_authorize(self):
         """始终允许电话授权"""
-        test_authorize = self.driver.find_element_by_id(tel_authorize)
+        test_authorize = self.driver.find_element_by_id(self.tel_authorize)
         test_authorize.click()
         time.sleep(1)
 
-    def test_close__reward(self):
+    def test_close_reward(self):
         """关闭新人红包奖励"""
-        close_reward = self.driver.find_element_by_android_uiautomator('new UiSelector().text(\"先去逛逛\")')
+        close_reward = self.driver.find_element_by_android_uiautomator('new UiSelector().text({})'.format(self.close_reward))
         close_reward[0].click()
 
 
     @classmethod
-    def tearDownClass(self):
-        self.driver.quit()
+    def tearDownClass(cls):
+        cls.driver.quit()
 
 
 # texture Testcase
 def suite():
     tests = [
-        "test_initial",
         "test_tel_authorize",
         "test_close_reward"
     ]
